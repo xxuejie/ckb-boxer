51f10f7e7d79127a798cd176f6845a9cc7f53f05
Xuejie Xiao <xxuejie@gmail.com>
2020-03-13 06:19:31 +0000

    Add simple protocol to process blocks

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
+use bytes::BytesMut;
 use ckb_app_config::{ExitCode, Setup};
 use ckb_build_info::Version;
-use ckb_chain::chain::ChainService;
-use ckb_logger::info;
-use ckb_shared::shared::SharedBuilder;
-use ckb_verification::{GenesisVerifier, Verifier};
+use ckb_chain::chain::{ChainController, ChainService};
+use ckb_logger::{error, info};
+use ckb_shared::{
+    shared::{Shared, SharedBuilder},
+    Snapshot,
+};
+use ckb_types::{
+    packed::{Block, BlockReader},
+    prelude::*,
+};
+use ckb_verification::{GenesisVerifier, HeaderResolverWrapper, HeaderVerifier, Verifier};
 use clap::{App, Arg, SubCommand};
-use std::sync::{Arc, Condvar, Mutex};
+use crossbeam_channel::select;
+use faster_hex::hex_decode;
+use std::io;
+use std::sync::Arc;
+use std::thread;
 
 fn main() {
     let matches = App::new("ckb-boxer")
@@ -51,20 +63,89 @@ fn main() {
         .expect("genesis verification failure");
 
     let chain_service = ChainService::new(shared.clone(), table);
-    let _chain_controller = chain_service.start(Some("ChainService"));
+    let chain_controller = chain_service.start(Some("ChainService"));
+
+    println!("0000TIPH{:016x}", shared.snapshot().tip_header().number());
+
+    let new_block_receiver = shared
+        .notify_controller()
+        .subscribe_new_block("BoxerBlockListener");
+    thread::Builder::new()
+        .name("BoxerBlockListener".to_string())
+        .spawn(move || loop {
+            select! {
+                recv(new_block_receiver) -> msg => match msg {
+                    Ok(block) => {
+                        println!("0000NBLK{:x}", block.hash());
+                    },
+                    Err(e) => {
+                        error!("Error listening for new blocks: {:?}", e);
+                        break;
+                    },
+                }
+            }
+        })
+        .expect("Start BoxerBlockListener thread failed");
 
     info!("ckb-boxer is now booted");
 
-    // Wait for exit
-    let exit = Arc::new((Mutex::new(()), Condvar::new()));
-    let e = Arc::clone(&exit);
-    ctrlc::set_handler(move || {
-        e.1.notify_all();
-    })
-    .expect("error setting Ctrl-C handler");
-    let _guard = exit
-        .1
-        .wait(exit.0.lock().expect("locking"))
-        .expect("waiting");
-    info!("exiting...");
+    loop {
+        let mut input = String::new();
+        match io::stdin().read_line(&mut input) {
+            Ok(_) => {
+                let input = input.trim();
+                if input.len() < 8 {
+                    error!("Invalid command: {}", input);
+                    continue;
+                }
+                let id = &input[0..4];
+                let method = &input[4..8];
+                let payload = &input[8..];
+                match method {
+                    "NBLK" => {
+                        if let Err(msg) =
+                            handle_new_block(&chain_controller, &shared, payload.as_bytes())
+                        {
+                            error!("Error processing new block: {}", msg);
+                        }
+                    }
+                    _ => {
+                        error!("Invalid method: {} for message: {}", method, id);
+                    }
+                }
+            }
+            Err(err) => {
+                error!("Error reading input: {:?}, exiting...", err);
+                break;
+            }
+        }
+    }
+}
+
+fn handle_new_block(
+    chain: &ChainController,
+    shared: &Shared,
+    payload: &[u8],
+) -> Result<(), String> {
+    let mut raw = BytesMut::with_capacity(payload.len() / 2);
+    hex_decode(payload, &mut raw).map_err(|e| format!("Hex decode error: {:?}", e))?;
+    let raw = raw.freeze();
+    BlockReader::verify(&raw, false)
+        .map_err(|e| format!("Molecule verification error: {:?}", e))?;
+    let block = Block::new_unchecked(raw).into_view();
+    let header = block.header();
+
+    // Verify header
+    let snapshot: &Snapshot = &shared.snapshot();
+    let resolver = HeaderResolverWrapper::new(&header, snapshot);
+    HeaderVerifier::new(snapshot, &shared.consensus())
+        .verify(&resolver)
+        .map_err(|e| format!("Header verification error: {:?}", e))?;
+
+    // Verify and insert block
+    chain
+        .process_block(Arc::new(block))
+        .map_err(|e| format!("Process block error: {:?}", e))?;
+
+    Ok(())
 }
